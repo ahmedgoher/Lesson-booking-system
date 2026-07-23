8e5d1a5ebedd1d0308fc8a5f3b805037851e05be
khaledashrafkhalil373 <khaledashrafkhalil373@gmail.com>
2026-07-23 17:20:59 +0300

    Add stdAttendence and settings

---
+++ Al-Muzayyen/Viewmodel/StudentAttendanceVM.cs
@@ -0,0 +1,12 @@
+﻿using Al_Muzayyen.Models;
+
+namespace Al_Muzayyen.Viewmodel
+{
+    public class StudentAttendanceVM
+    {
+        public DateTime Date { get; set; }
+        public AttendanceStatus IsPresent { get; set; }
+        public HomeworkStatus Homework { get; set; }
+        public string? Notes { get; set; }
+    }
+}
