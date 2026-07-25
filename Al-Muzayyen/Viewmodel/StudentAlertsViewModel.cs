57d172d185fac5e8aac07cfeff0f066ec2d8c942
Ahmed morsi <morsi6540@gmail.com>
2026-07-25 14:27:30 +0300

    add main page

---
+++ Al-Muzayyen/Viewmodel/StudentAlertsViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Al_Muzayyen.Models;
+
+namespace Al_Muzayyen.Viewmodel
+{
+    public class StudentAlertsViewModel
+    {
+        public List<Exam>? PendingExams { get; set; }
+        public GroupChangeRequest? GroupRequest { get; set; }
+    }
+}
