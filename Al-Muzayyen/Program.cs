7f363bd3bc54a1ee6d247c72d2a7288bf8d8d5d2
Ahmed morsi <morsi6540@gmail.com>
2026-07-26 18:11:41 +0300

    change image

--- Al-Muzayyen/Program.cs
+++ Al-Muzayyen/Program.cs
@@ -243,7 +243,7 @@ namespace Al_Muzayyen
                     };
 
                     // تشفير كلمة المرور وتعيينها
-                    admin.Password = hasher.HashPassword(admin, "123456");
+                    admin.Password = hasher.HashPassword(admin, "Admin123");
 
                     context.Admins.Add(admin);
                     context.SaveChanges();
